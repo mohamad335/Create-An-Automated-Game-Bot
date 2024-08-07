59492b91c2818e8bc15b908b4d4bb4f6b7e41be2
Mohammad <mhamdl2amen@gmail.com>
2024-08-07 18:13:42 +0300

    better arrangement

--- main.py
+++ main.py
@@ -8,40 +8,42 @@ options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=options)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, "cookie")
-items = driver.find_elements(by=By.CSS_SELECTOR, value="#store div")
-item_ids = [item.get_attribute("id") for item in items]
-time_out = time.time() + 5
-five_min = time.time() + 60*5
+items=driver.find_elements(by=By.CSS_SELECTOR, value="#store div")
+items_ids=[ item.get_attribute("id") for item in items]
+time_out= time.time() + 5
+five_minutes = time.time() + 5*60
 while True:
     cookie.click()
-    if time.time() > time_out:
-        all_prices = driver.find_elements(By.CSS_SELECTOR, value="#store b")
-        item_prices = []
-        for price in all_prices:
-            element_text = price.text
-            if element_text != "":
-                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+    if time.time()> time_out:
+        prices = driver.find_elements(by=By.CSS_SELECTOR, value="#store b")
+        item_prices=[]
+        for price in prices:
+            price_text = price.text
+            if price_text !="":
+                cost = int(price_text.split("-")[1].strip().replace(",", ""))
                 item_prices.append(cost)
-        cookie_upgrades = {}
+        cookie_dictionary={}
         for n in range(len(item_prices)):
-            cookie_upgrades[item_prices[n]] = item_ids[n]
-        money_element = driver.find_element(By.ID, "money").text
-        if "," in money_element:
-            money_element = money_element.replace(",", "")
-        cookie_count = int(money_element)
-        affordable_upgrades = {}
-        for cost, id in cookie_upgrades.items():
-            if cookie_count > cost:
-                affordable_upgrades[cost] = id
-        highest_price_affordable_upgrade = max(affordable_upgrades)
-        print(highest_price_affordable_upgrade)
-        to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
-        driver.find_element(By.ID, to_purchase_id).click()
-        time_out = time.time() + 5
-        if time.time() > five_min:
-            cookie_per_s = driver.find_element(By.ID, "cps").text
-            print(cookie_per_s)
+            cookie_dictionary[item_prices[n]]=items_ids[n]
+        money=driver.find_element(By.ID,"money").text
+        if "," in money:
+            money=money.replace(",","")
+        money_count=int(money)
+        affordable_upgrades={}
+        for cost, id in cookie_dictionary.items():
+            if money_count>cost:
+                affordable_upgrades[cost]=id
+        highest_price=max(affordable_upgrades)
+        print(highest_price)
+        price_id=affordable_upgrades[highest_price]
+        driver.find_element(By.ID,price_id).click()
+        time_out=time.time()+5
+        if time.time()>five_minutes:
+            cookies_perSecond = driver.find_element(By.ID,"cps").text
+            print(cookies_perSecond)
             break
+            
+
 
 
 
